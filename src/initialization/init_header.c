2eb0351d0df24dcde097c49af223e50223f6405d
Ghali Boucetta <gboucett@student.42.fr>
2024-08-08 01:00:55 +0200

    add title and icon to header, title may need to be generated using FreeTypes

--- src/initialization/init_header.c
+++ src/initialization/init_header.c
@@ -1,5 +1,33 @@
+#include <SDL_image.h>
+
 #include "defines.h"
 #include "minesweeper.h"
+
+
+static SDL_Texture *get_window_title()
+{
+	TTF_Font *font = TTF_OpenFont("resources/NotoSans.ttf", 35);
+	char *    dup  = strdup(minesweeper.title);
+	dup[0]         = toupper(dup[0]);
+
+	SDL_Surface *surface = TTF_RenderUTF8_Blended(font, dup, (SDL_Color){255, 255, 255, 255});
+
+	TTF_CloseFont(font);
+	free(dup);
+
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(minesweeper.renderer, surface);
+	SDL_FreeSurface(surface);
+	return tex;
+}
+
+static SDL_Texture *get_icon()
+{
+	SDL_Surface *icon = IMG_Load("resources/icon.png");
+
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(minesweeper.renderer, icon);
+	SDL_FreeSurface(icon);
+	return tex;
+}
 
 
 void init_header(void)
@@ -10,6 +38,8 @@ void init_header(void)
 	hdr.rect.w = minesweeper.win_width;
 	hdr.rect.h = HEIGHT_UI_HEADER;
 	hdr.bg     = apply_grad(minesweeper.renderer, hdr.rect, &grad_ui_header);
+	hdr.title  = get_window_title();
+	hdr.icon   = get_icon();
 
 	minesweeper.hdr = hdr;
 }
